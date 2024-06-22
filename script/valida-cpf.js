8d5b938a231d37578732290ffa4f44866ff27352
Marcos Marostega <103620713+marostegaf@users.noreply.github.com>
2024-06-22 19:17:01 -0300

    v1.0

---
+++ script/valida-cpf.js
@@ -0,0 +1,51 @@
+export default function cpfValido(campo) {
+    const cpf = campo.value.replace(/\.|-/g, "");
+    if (validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)) {
+        campo.setCustomValidity("Esse CPF é inválido!")
+    }
+}
+
+function validaNumerosRepetidos(cpf) {
+    const numerosRepetidos = [
+        "00000000000",
+        "11111111111"
+    ];
+
+    return numerosRepetidos.includes(cpf);
+}
+
+function validaPrimeiroDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 10;
+
+    for(let tamanho = 0; tamanho < 9; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+    }
+
+    return soma != cpf[9];
+}
+
+function validaSegundoDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 11;
+
+    for(let tamanho = 0; tamanho < 10; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+    }
+
+    return soma != cpf[10];
+}
